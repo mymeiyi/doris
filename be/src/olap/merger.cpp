e09f2b3a591618700f0b13050a3bc2648e57b6b0
meiyi <myimeiyi@gmail.com>
2024-09-04 18:58:56 +0800

    debug

--- be/src/olap/merger.cpp
+++ be/src/olap/merger.cpp
@@ -210,6 +210,22 @@ void Merger::vertical_split_columns(const TabletSchema& tablet_schema,
             }
         }
     }
+    std::stringstream ss;
+    ss << "index to cid: ";
+    for (auto idx = 0; idx < tablet_schema.columns().size(); ++idx) {
+        ss << "[" << idx << ": " << tablet_schema.column(idx).unique_id() << "] ";
+    }
+    std::stringstream ss1;
+    for (const auto& id : tablet_schema.cluster_key_idxes()) {
+        ss1 << id << " ";
+    }
+    std::stringstream ss2;
+    for (const auto& item : key_columns) {
+        ss2 << item << " ";
+    }
+    LOG(INFO) << "sout: vertical_split_columns, table_id=" << tablet_schema.table_id()
+              << ", tablet_schema=" << ss.str() << ", cluster key ids=" << ss1.str()
+              << ", key columns=" << ss2.str();
     VLOG_NOTICE << "sequence_col_idx=" << sequence_col_idx
                 << ", delete_sign_idx=" << delete_sign_idx;
     // for duplicate no keys
@@ -457,6 +473,11 @@ Status Merger::vertical_merge_rowsets(BaseTabletSPtr tablet, ReaderType reader_t
     std::vector<uint32_t> key_group_cluster_key_idxes;
     _generate_key_group_cluster_key_idxes(tablet_schema, column_groups,
                                           key_group_cluster_key_idxes);
+    std::stringstream ss3;
+    for (const auto item : key_group_cluster_key_idxes) {
+        ss3 << item << " ";
+    }
+    LOG(INFO) << "sout: vertical_split_columns, cluster key idxes=" << ss3.str();
 
     vectorized::RowSourcesBuffer row_sources_buf(
             tablet->tablet_id(), dst_rowset_writer->context().tablet_path, reader_type);
