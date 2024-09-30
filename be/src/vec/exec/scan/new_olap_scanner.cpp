628c5075fde1664b3203fe8e1ea1ebae53e46423
meiyi <myimeiyi@gmail.com>
2024-09-30 11:30:44 +0800

    r

--- be/src/vec/exec/scan/new_olap_scanner.cpp
+++ be/src/vec/exec/scan/new_olap_scanner.cpp
@@ -395,6 +395,7 @@ Status NewOlapScanner::_init_tablet_reader_params(
         !(_state->skip_delete_bitmap() || _state->query_mow_in_mor())) {
         _tablet_reader_params.delete_bitmap = &tablet->tablet_meta()->delete_bitmap();
     }
+    _tablet_reader_params.query_mow_in_mor = _state->query_mow_in_mor();
     /*LOG(INFO) << "sout: skip merge=" << _state->skip_storage_engine_merge()
               << ", query_id=" << print_id(_state->query_id())
               << ", tablet_id=" << tablet->tablet_id() << ", tablet=" << tablet;*/
