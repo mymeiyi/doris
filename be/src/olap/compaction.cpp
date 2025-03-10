8a6045e33a0f4e70f7108c98a70e9aa94896b21f
meiyi <meiyi@selectdb.com>
2025-03-10 15:52:08 +0800

    r

--- be/src/olap/compaction.cpp
+++ be/src/olap/compaction.cpp
@@ -1171,6 +1171,10 @@ Status CompactionMixin::modify_rowsets() {
                                     });
             }
 
+            LOG(INFO) << "sout: cumulative compaction: the merged rows(" << _stats.merged_rows
+                      << "), filtered rows(" << _stats.filtered_rows
+                      << ") is not equal to missed rows(" << missed_rows_size
+                      << ") in rowid conversion, tablet_id: " << _tablet->tablet_id();
             if (_tablet->tablet_state() == TABLET_RUNNING &&
                 merged_missed_rows_size != missed_rows_size && need_to_check_missed_rows) {
                 std::stringstream ss;
