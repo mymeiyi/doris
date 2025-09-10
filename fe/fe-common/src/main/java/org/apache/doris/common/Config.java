82e71a751b288d0e909bac0869e3ccc91e7cb8d1
meiyi <meiyi@selectdb.com>
2025-09-10 10:23:18 +0800

    test aksk

--- fe/fe-common/src/main/java/org/apache/doris/common/Config.java
+++ fe/fe-common/src/main/java/org/apache/doris/common/Config.java
@@ -21,6 +21,13 @@ import java.io.File;
 
 public class Config extends ConfigBase {
 
+    @ConfField
+    public static String ak = "";
+    @ConfField
+    public static String sk = "";
+    @ConfField
+    public static String bucket = "";
+
     @ConfField(description = {"用户自定义配置文件的路径，用于存放 fe_custom.conf。该文件中的配置会覆盖 fe.conf 中的配置",
             "The path of the user-defined configuration file, used to store fe_custom.conf. "
                     + "The configuration in this file will override the configuration in fe.conf"})
