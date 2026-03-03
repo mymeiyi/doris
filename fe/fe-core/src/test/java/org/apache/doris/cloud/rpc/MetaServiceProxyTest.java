fa311cb5876c6e5d2e376e941c4d62a10a6d8720
meiyi <meiyi@selectdb.com>
2026-03-03 11:45:32 +0800

    fix

--- fe/fe-core/src/test/java/org/apache/doris/cloud/rpc/MetaServiceProxyTest.java
+++ fe/fe-core/src/test/java/org/apache/doris/cloud/rpc/MetaServiceProxyTest.java
@@ -71,7 +71,7 @@ public class MetaServiceProxyTest {
         lastConnTimeMs.add(0L);
 
         MetaServiceProxy.MetaServiceClientWrapper wrapper = Deencapsulation.getField(proxy, "w");
-        String response = wrapper.executeRequest("ignored", 0, (ignored) -> "ok",
+        String response = wrapper.executeRequest("ignored", (ignored) -> "ok", 0,
                 (res) -> (Cloud.MetaServiceResponseStatus.newBuilder()).build());
         Assert.assertEquals("ok", response);
         Mockito.verify(client, Mockito.never()).shutdown(Mockito.anyBoolean());
@@ -94,9 +94,9 @@ public class MetaServiceProxyTest {
 
         MetaServiceProxy.MetaServiceClientWrapper wrapper = Deencapsulation.getField(proxy, "w");
         try {
-            wrapper.executeRequest("ignored", 0, (ignored) -> {
+            wrapper.executeRequest("ignored", (ignored) -> {
                 throw new RuntimeException("rpc failed");
-            }, (res) -> (Cloud.MetaServiceResponseStatus.newBuilder()).build());
+            }, 0, (res) -> (Cloud.MetaServiceResponseStatus.newBuilder()).build());
             Assert.fail("should throw RpcException");
         } catch (RpcException ignored) {
             // expected
