d3a9bc8692f529d25e85cc464de3f8d230680c39
meiyi <meiyi@selectdb.com>
2026-03-12 17:44:01 +0800

    [fix](cloud) checkpoint save cloud version and tablet stats to image

--- fe/fe-core/src/main/java/org/apache/doris/catalog/CloudTabletStatMgr.java
+++ fe/fe-core/src/main/java/org/apache/doris/catalog/CloudTabletStatMgr.java
@@ -59,6 +59,12 @@ public class CloudTabletStatMgr extends MasterDaemon {
 
     @Override
     protected void runAfterCatalogReady() {
+        if (cloudTableStatsList.isEmpty()) {
+            // use tablet stats loaded from image to update table stats when fe start
+            // avoid that the table stats is empty for a long time since getAllTabletStats may consume a long time
+            LOG.info("cloud tablet stat is empty, will update stat info of all tables");
+            updateStatInfo(Env.getCurrentInternalCatalog().getDbIds());
+        }
         LOG.info("cloud tablet stat begin");
         List<Long> dbIds = getAllTabletStats();
         updateStatInfo(dbIds);
