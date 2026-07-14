6d5e2ae4c87eec09ba2675fac23e6da9f6d58d5a
meiyi <meiyi@selectdb.com>
2026-07-14 17:08:08 +0800

    [fix](debug) add module filter for debug points

--- be/src/util/debug_points.cpp
+++ be/src/util/debug_points.cpp
@@ -17,10 +17,74 @@
 
 #include "util/debug_points.h"
 
+#include <algorithm>
+#include <cctype>
+#include <sstream>
+#include <string>
+#include <string_view>
+
 #include "common/logging.h"
 #include "util/time.h"
 
 namespace doris {
+namespace {
+
+constexpr uint64_t UNINITIALIZED_DEBUG_POINT_MODULE_MASK = UINT64_MAX;
+
+constexpr uint64_t all_debug_point_modules_mask() {
+    return (1ULL << static_cast<uint8_t>(DebugPointModule::MAX)) - 1;
+}
+
+std::atomic<uint64_t> g_enabled_debug_point_module_mask {UNINITIALIZED_DEBUG_POINT_MODULE_MASK};
+
+std::string trim_module_name(std::string_view value) {
+    auto begin = value.begin();
+    auto end = value.end();
+    while (begin != end && std::isspace(static_cast<unsigned char>(*begin))) {
+        ++begin;
+    }
+    while (begin != end && std::isspace(static_cast<unsigned char>(*(end - 1)))) {
+        --end;
+    }
+    std::string result(begin, end);
+    std::ranges::transform(result, result.begin(),
+                           [](unsigned char c) { return static_cast<char>(std::tolower(c)); });
+    return result;
+}
+
+uint64_t debug_point_module_mask(std::string_view modules) {
+    uint64_t mask = 0;
+    std::stringstream ss {std::string(modules)};
+    std::string module;
+    while (std::getline(ss, module, ',')) {
+        module = trim_module_name(module);
+        if (module.empty()) {
+            continue;
+        }
+        if (module == "*" || module == "all") {
+            return all_debug_point_modules_mask();
+        }
+        if (module == "compaction") {
+            mask |= 1ULL << static_cast<uint8_t>(DebugPointModule::COMPACTION);
+        } else if (module == "file_reader") {
+            mask |= 1ULL << static_cast<uint8_t>(DebugPointModule::FILE_READER);
+        } else if (module == "load") {
+            mask |= 1ULL << static_cast<uint8_t>(DebugPointModule::LOAD);
+        } else if (module == "scan") {
+            mask |= 1ULL << static_cast<uint8_t>(DebugPointModule::SCAN);
+        } else {
+            LOG(WARNING) << "unknown debug point module: " << module;
+        }
+    }
+    return mask;
+}
+
+config::RegisterConfUpdateCallback reg_update_debug_point_modules(
+        "enable_debug_point_modules", [](const void*, const void* new_ptr) {
+            DebugPoints::update_enabled_modules(*reinterpret_cast<const std::string*>(new_ptr));
+        });
+
+} // namespace
 
 DebugPoints::DebugPoints() : _debug_points(std::make_shared<const DebugPointMap>()) {}
 
@@ -53,6 +117,25 @@ std::shared_ptr<DebugPoint> DebugPoints::get_debug_point(const std::string& name
     }
 
     return debug_point;
+}
+
+bool DebugPoints::is_module_enabled(DebugPointModule module) {
+    if (!config::enable_debug_points) {
+        return false;
+    }
+    auto bit = 1ULL << static_cast<uint8_t>(module);
+    auto mask = g_enabled_debug_point_module_mask.load(std::memory_order_relaxed);
+    if (UNLIKELY(mask == UNINITIALIZED_DEBUG_POINT_MODULE_MASK)) {
+        update_enabled_modules(config::enable_debug_point_modules);
+        mask = g_enabled_debug_point_module_mask.load(std::memory_order_relaxed);
+    }
+    return (mask & bit) != 0;
+}
+
+void DebugPoints::update_enabled_modules(const std::string& modules) {
+    auto mask = debug_point_module_mask(modules);
+    g_enabled_debug_point_module_mask.store(mask, std::memory_order_relaxed);
+    LOG(INFO) << "set enable_debug_point_modules=" << modules << ", mask=" << mask;
 }
 
 void DebugPoints::add(const std::string& name, std::shared_ptr<DebugPoint> debug_point) {
