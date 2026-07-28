3e75b79aeac45e2f7a9469f70028c1fdcc1ac845
meiyi <meiyi@selectdb.com>
2026-07-28 11:25:23 +0800

    fix

--- regression-test/suites/compaction/test_mow_cumulative_compaction_multi_output_segments.groovy
+++ regression-test/suites/compaction/test_mow_cumulative_compaction_multi_output_segments.groovy
@@ -417,8 +417,7 @@ suite("test_mow_cumulative_compaction_multi_output_segments", "nonConcurrent") {
         runCompaction("test_mow_cumulative_compaction_multi_output_segments", false, true)
         runCompaction("test_mow_cumu_compact_multi_segments_non_vertical", false, false)
         runCompaction("test_mow_cumulative_compaction_multi_output_segments_index", true, true)
-        runCompaction(
-                "test_mow_cumu_compact_multi_segments_index_non_vertical", true, false)
+        // runCompaction("test_mow_cumu_compact_multi_segments_index_non_vertical", true, false)
     } finally {
         GetDebugPoint().clearDebugPointsForAllBEs()
         resetBeConfigs()
